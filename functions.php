a8ecd3ae7abb175245f88d692728514a3c7de680
Mike <michael.meservy@gmail.com>
2021-03-21 18:46:40 -0600

    added script to defer all js

--- functions.php
+++ functions.php
@@ -40,3 +40,13 @@ function prefix_enqueue_awesome() {
 		'5.3.0' 
 	);
 }
+
+
+function defer_parsing_of_js($url)
+{
+  if (is_admin()) return $url; //don't break WP Admin
+  if (false === strpos($url, '.js')) return $url;
+  if (strpos($url, 'jquery.js')) return $url;
+  return str_replace(' src', ' defer src', $url);
+}
+add_filter('script_loader_tag', 'defer_parsing_of_js', 10);
